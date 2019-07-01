ed8ecefcaf3b3ef1445467e3efb6b8757bf6bdfe
ajaykumar2919 <52315704+ajaykumar2919@users.noreply.github.com>
2019-07-01 10:57:39 +0530

    Create 3program.py

---
+++ 3program.py
@@ -0,0 +1,7 @@
+Aj=input()
+if Aj in ('a','e','i','o','u'):
+   print("Vowel")
+elif Aj in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print("Consonant")
+else: 
+   print("Invalid")
