251aa9bf039cf4dab9dd0017e8d7448b4087456b
ajaykumar2919 <52315704+ajaykumar2919@users.noreply.github.com>
2019-07-01 11:06:33 +0530

    Create 4program.py

---
+++ 4program.py
@@ -0,0 +1,5 @@
+a=input("")
+if a=='a'or a=='b'or a=='c'or a=='d'or a=='e'or a=='f'or a=='g'or a=='h'or a=='i'or a=='j'or a=='k'or a=='l'or a=='m'or a=='n'or a=='o'or a=='p'or a=='q'or a=='r'or a=='s'or a=='t'or a=='u'or a=='v'or a=='w'or a=='x'or a=='y'or a=='z'or a=='A'or a=='B'or a=='C'or a=='D'or a=='E'or a=='F'or a=='G'or a=='H'or a=='I'or a=='J'or a=='K'or a=='L'or a=='M'or a=='N'or a=='O'or a=='P'or a=='R'or a=='S'or a=='t'or a=='U'or a=='V'or  a=='W'or a=='X'or a=='Y'or a=='Z':
+    print("Alphabet")
+else:
+    print("No")
